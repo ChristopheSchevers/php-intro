fef3835611cfd40593300488ba61b8692c8607ec
Christophe Schevers <christophe.schevers@gmail.com>
2019-05-16 18:12:48 +0200

    work so far

--- working-files/5.Functions/security.php
+++ working-files/5.Functions/security.php
@@ -1,3 +1,20 @@
 <?php
 
 ?>
+
+<form action="home.php" method="post">
+    <div class="form-group">
+        <div class="row">
+            <p>Are you a bot from the future coming to exterminate the human race and are just here for a cool nickname?</p>
+        </div>
+        <div class="row">
+            <input class="form-check-input" type="radio" name="radio1" value="yes">
+            <label for="radio1" class="form-check-label">Yes</label>
+            <input class="form-check-input" type="radio" name="radio2" value="no">
+            <label for="radio2" class="form-check-label">Yes</label>
+        </div>
+        <div class="row justify-content-center">
+            <button type="submit" class="btn btn-secondary">Submit</button>
+        </div>
+    </div>
+</form>
